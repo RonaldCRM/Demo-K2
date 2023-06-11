82cb0e376051e4303b99ac2b10eef67802215be1
ronaldo.maciel <ronaldo.r.maciel@gmail.com>
2023-06-11 00:52:40 -0300

    decimo nono commít

--- index.php
+++ index.php
@@ -6,34 +6,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tela de login</title>
     <style>
-        body{
+        body {
             font-family: Arial, Helvetica, sans-serif;
             background-image: linear-gradient(45deg, black, black);
+            margin: 0;
         }
-        div{
+        div {
             background-color: rgba(256, 256, 256, 0.9);
             position: absolute;
             top: 50%;
             left: 25%;
-            transform: translate(-50%,-50%);
+            transform: translate(-50%, -50%);
             padding: 60px;
             border-radius: 15px;
             color: black;
         }
-        img {           
-            position: absolute;
-            top: 90%;
-            left: 90%;
-            transform: translate(-50%,-50%);          
-            width: auto;
+        img {
+            position: fixed;
+            top: 20px;
+            left: 20px;
+            width: 100px;
             height: auto;
         }
     </style>
 </head>
 <body>
+    <img src="Logo.png" alt="Logo K2">
     <div>
         <h1>Faça Parte dessa Equipe Campeã !!!</h1>
-        <img src=Logo.png alt="Logo K2">
     </div>
 </body>
 </html>
