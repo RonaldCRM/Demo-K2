233a8a6aaef3bbb169691c100df2056006ce3955
ronaldo.maciel <ronaldo.r.maciel@gmail.com>
2023-06-13 17:15:27 -0300

    oitavo commit demo

--- index.php
+++ index.php
@@ -40,8 +40,8 @@
 <body>
     <img src="Logo.png" alt="Logo K2" class="logo-img">
     <div>
-        <h1>Faça parte dessa equipe Campeã !!!</h1>
+        <h1>Eu faço parte dessa escalada digital !!!</h1>
     </div>
-    <img src="evento.jpg" alt="foto evento " class="evento">
+    <img src="" alt="foto evento " class="evento">
 </body>
 </html>
