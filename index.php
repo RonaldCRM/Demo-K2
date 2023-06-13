1e8d0bfb919fea34ec7374ab1699d95abbc4042f
ronaldo.maciel <ronaldo.r.maciel@gmail.com>
2023-06-13 16:34:16 -0300

    segundo commit demo

--- index.php
+++ index.php
@@ -42,6 +42,6 @@
     <div>
         <h1>Faça parte dessa equipe Campeã !!!</h1>
     </div>
-    <img src="" alt="foto evento " class="s3">
+    <img src=" " alt="foto evento " class="s3">
 </body>
 </html>
